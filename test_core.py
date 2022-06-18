1bb1b4c5c260f40e01ebd28d5cb7e798ae9797ca
Tarun Kumar <ttarunkkumar99@gmail.com>
2022-06-19 01:20:55 +0500

    make tests more functional

--- test_core.py
+++ test_core.py
@@ -3,8 +3,7 @@ from dataclasses import FrozenInstanceError
 from core import Sider
 
 def test_sider_is_a_store():
-    sider = Sider()
-    assert sider._store == {}
+    assert Sider()._store == {}
 
 def test_sider_store_is_immutable():
     sider = Sider()
@@ -13,7 +12,11 @@ def test_sider_store_is_immutable():
 
 def test_sider_construction_with_existing_store():
     old_store = {'2': 'b','o':'r', '2': 'b'}
-    sider = Sider.from_dict(old_store)
-    assert sider._store == old_store # value is same
-    assert sider._store is not old_store # reference is different
+    assert Sider.from_dict(old_store)._store == old_store # value is same
+    assert Sider.from_dict(old_store)._store is not old_store # reference is different.
+
+def test_sider_equivalence():
+    old_store = {'2': 'b','o':'r', '2': 'b'}
+    assert Sider.from_dict(old_store) == Sider.from_dict(old_store)
+
 
