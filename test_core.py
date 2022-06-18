d758aaa2cec64e8f6105d5e77d7d314758269d11
Tarun Kumar <ttarunkkumar99@gmail.com>
2022-06-18 19:52:15 +0500

    test immutability

--- test_core.py
+++ test_core.py
@@ -1,5 +1,13 @@
+import pytest
+from dataclasses import FrozenInstanceError
 from core import Sider
 
 def test_sider_is_a_store():
     sider = Sider()
     assert sider.store == {}
+
+def test_sider_store_is_immutable():
+    sider = Sider()
+    assert sider.store == {}
+    with pytest.raises(FrozenInstanceError):
+        sider.store = {'2': 'b','o':'r', '2': 'b'}
