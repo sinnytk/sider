51859da3e30f39b0c57e8caf7af1dffadba7aae6
Tarun Kumar <ttarunkkumar99@gmail.com>
2022-06-19 01:24:05 +0500

    format with black

--- test_core.py
+++ test_core.py
@@ -2,21 +2,25 @@ import pytest
 from dataclasses import FrozenInstanceError
 from core import Sider
 
+
 def test_sider_is_a_store():
     assert Sider()._store == {}
+
 
 def test_sider_store_is_immutable():
     sider = Sider()
     with pytest.raises(FrozenInstanceError):
-        sider._store = {'2': 'b','o':'r', '2': 'b'}
+        sider._store = {"2": "b", "o": "r", "2": "b"}
+
 
 def test_sider_construction_with_existing_store():
-    old_store = {'2': 'b','o':'r', '2': 'b'}
-    assert Sider.from_dict(old_store)._store == old_store # value is same
-    assert Sider.from_dict(old_store)._store is not old_store # reference is different.
+    old_store = {"2": "b", "o": "r", "2": "b"}
+    assert Sider.from_dict(old_store)._store == old_store  # value is same
+    assert Sider.from_dict(old_store)._store is not old_store  # reference is different.
+
 
 def test_sider_equivalence():
-    old_store = {'2': 'b','o':'r', '2': 'b'}
+    old_store = {"2": "b", "o": "r", "2": "b"}
     assert Sider.from_dict(old_store) == Sider.from_dict(old_store)
 
 
